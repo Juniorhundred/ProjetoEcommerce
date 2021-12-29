82d3b3e7ca3fb182bb8da9cb2620a5d7fb4a7b20
SandroJunior <juniorfuca2012@gmail.com>
2021-12-29 19:12:47 -0300

    Conserto Mock

--- src/main/java/br/com/brq/projetoecommerce/utils/MockUtil.java
+++ src/main/java/br/com/brq/projetoecommerce/utils/MockUtil.java
@@ -1,6 +1,5 @@
 package br.com.brq.projetoecommerce.utils;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,126 +30,121 @@ import br.com.brq.projetoecommerce.services.VendaService;
 
 @Component
 public class MockUtil {
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private ImagemService imagemService;
-	
+
 	@Autowired
 	private ItemVendaService itemVendaService;
-	
+
 	@Autowired
 	private VendaService vendaService;
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Autowired
 	private EnderecoService enderecoService;
-	
-	
-	
+
 	public CategoriaDTO categoriaControllerMock() {
-		
-		CategoriaEntity categoriaMocka = categoriaService.salvar(CategoriaEntity.builder().nomeCategoria("Eletronicos").build());		
+
+		CategoriaEntity categoriaMocka = categoriaService
+				.salvar(CategoriaEntity.builder().nomeCategoria("Eletronicos").build());
 		return categoriaMocka.toDTO();
 	}
-	
+
 	public ImagemDTO imagemControllerMock() {
-		
-		ImagemEntity imagemMock = imagemService.salvar(ImagemEntity.builder().imagemProduto("(*-*)").build());		
+
+		ImagemEntity imagemMock = imagemService.salvar(ImagemEntity.builder().imagemProduto("(*-*)").build());
 		return imagemMock.toDTO();
 	}
-	
+
 	public ProdutoDTO produtoControllerMock() {
-		
+
 		CategoriaDTO categoriaMock = this.categoriaControllerMock();
-	    
+
 		List<CategoriaEntity> listCat = new ArrayList<>();
 		listCat.add(categoriaMock.toEntity());
-		
-		ImagemDTO imagemMock =  this.imagemControllerMock();		
-		
+
+		ImagemDTO imagemMock = this.imagemControllerMock();
+
 		List<ImagemEntity> listIma = new ArrayList<>();
 		listIma.add(imagemMock.toEntity());
-		
-		ProdutoEntity produtoMock = produtoService.salvar(ProdutoEntity.builder().nome("Xiaomi").preco(2000).descricao("Celular")
-				.categorias(listCat).imagens(listIma).build());	
-		
+
+		ProdutoEntity produtoMock = produtoService.salvar(ProdutoEntity.builder().nome("Xiaomi").preco(2000)
+				.descricao("Celular").categorias(listCat).imagens(listIma).build());
+
 		return produtoMock.toDTO();
 	}
-	
+
 	public ItemVendaDTO itemControllerMock() {
-		
-		ProdutoDTO produtoMock = this.produtoControllerMock();	
+
+		ProdutoDTO produtoMock = this.produtoControllerMock();
 		List<ProdutoEntity> listProd = new ArrayList<>();
 		listProd.add(produtoMock.toEntity());
-		
-		ItemVendaEntity itemMock = itemVendaService.salvar(ItemVendaEntity.builder().itemQuantidade(1).itemProduto(listProd).build());
-		
+
+		ItemVendaEntity itemMock = itemVendaService
+				.salvar(ItemVendaEntity.builder().itemQuantidade(1).itemProduto(listProd).build());
+
 		return itemMock.toDTO();
 	}
-	
+
 	private UsuarioDTO usuarioMock() {
 		EnderecoDTO dto = enderecoMock();
 		List<EnderecoEntity> listDto = new ArrayList<>();
 		listDto.add(dto.toEntity());
-		UsuarioEntity enty = usuarioService.salvar(UsuarioEntity.builder().nome("Karina")
-		.cpf("12345678911")
-		.celular("11222223333")
-		.telefone("22223333")
-		.email("boladinhos@gmail.com")
-		.dataDeNascimento(LocalDate.of(1998, 05, 18)).enderecos(listDto)
-		.build());
+		UsuarioEntity enty = usuarioService.salvar(
+				UsuarioEntity.builder().nome("Karina").cpf("12345678911").celular("11222223333").telefone("22223333")
+						.email("boladinhos@gmail.com").dataDeNascimento("1998, 05, 18").enderecos(listDto).build());
 		return enty.toDTO();
-		}
-	
+	}
+
 	private EnderecoDTO enderecoMock() {
-		EnderecoEntity enty = enderecoService.salvar(EnderecoEntity.builder()
-				.enderecoId(1)
+		EnderecoEntity enty = enderecoService.salvar(EnderecoEntity.builder().enderecoId(1)
 				.logradouro("Rua do Anderson").numero("55").complemento("ap 12").cep("91234-567")
-				.cidade("Cidade do Anderson").bairro("Bairro dos boladinhos").estado("estado laico").build());	
-		
-		return enty.toDTO();		
-		}
-	
+				.cidade("Cidade do Anderson").bairro("Bairro dos boladinhos").estado("estado laico").build());
+
+		return enty.toDTO();
+	}
+
 	public VendaDTO vendaControllerMock() {
-		
+
 		ItemVendaDTO itemMock = this.itemControllerMock();
 		List<ItemVendaEntity> listItem = new ArrayList<>();
 		listItem.add(itemMock.toEntity());
-		
+
 		UsuarioDTO uDto = usuarioMock();
 		UsuarioEntity uEnty = uDto.toEntity();
-		
-		VendaEntity vendaMock = vendaService.salvar(VendaEntity.builder().dataVenda("24-12-2021").itens(listItem).usuario(uEnty).build());
-			
+
+		VendaEntity vendaMock = vendaService
+				.salvar(VendaEntity.builder().dataVenda("24-12-2021").itens(listItem).usuario(uEnty).build());
+
 		return vendaMock.toDTO();
 	}
-	
+
 	public CategoriaEntity categoriaMock() {
 		return CategoriaEntity.builder().nomeCategoria("Eletronico").build();
 	}
-	
+
 	public ImagemEntity imagemMock() {
 		return ImagemEntity.builder().imagemProduto("nlousafvcijiuasfv").build();
 	}
-	
 
 	public ItemVendaEntity itemVendaMock() {
 		return ItemVendaEntity.builder().idItemVenda(2).itemQuantidade(1).build();
 	}
-	
+
 	public ProdutoEntity produtoMock() {
 		return ProdutoEntity.builder().nome("Xiaomi").preco(2000).descricao("Celular Chines").build();
 	}
-	
+
 	public VendaEntity vendaMock() {
-		return VendaEntity.builder().build(); 
+		return VendaEntity.builder().build();
 	}
 }
